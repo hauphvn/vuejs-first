617d0880048f250c8ff00e7511a068105b9bd40e
hauphvn <pvhau@cmc.com.vn>
2021-12-01 13:48:19 +0700

    Clearing old project

--- src/router.js
+++
@@ -1,8 +0,0 @@
-import User from "@/components/user/User";
-import UserForm from "@/components/form/UserForm";
-
-export const routes = [
-    {path: '/', name: 'user', component: User},
-    {path: '/form', name: 'form', component: UserForm}
-];
-
